befd99478e6cd1629d98c841a540dd79ff29d44c
LuizPulino <luizpulino@gmail.com>
2024-04-02 17:45:14 -0300

    3

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
 const text = document.getElementById('text');
-text.innerHTML = 'Hello World!!';
+const start = document.getElementById('start');
+const synth = window.speechSynthesis;
+const utterance = new SpeechSynthesisUtterance(text.innerText);
+utterance.lang = 'pt-BR';
+utterance.rate = 1;
+utterance.speed = 1;
+start.addEventListener('click', () => {
+  synth.speak(utterance);
+});
